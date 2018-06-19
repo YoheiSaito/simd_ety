35c1e59a14dc85f06bcc6401f79899847745f2e0
YoheiSaito <teyo.13324@gmail.com>
2018-06-19 15:33:35 +0900

    add comment

--- hello-world/simd.c
+++ hello-world/simd.c
@@ -3,21 +3,30 @@
 #include <stdlib.h>
 #include <immintrin.h>//AVX: -mavx
 
-void vec_add(const size_t n, double *z, const double *x, const double *y){
+
+void vec_add(const size_t n, double *z, const double *x, const double *y)
+{	
+	//double_sizeはSIMD1本128bitに4つのdouble(32bit)が入るため
+	//1024のdoubleを128ビットのデータ配列に収めると配列の長さは1024/4になる.
 	static const size_t double_size = 4;
 	const size_t end = n / double_size;
-
+	
 	__m256d *vz = (__m256d *)z;
 	__m256d *vx = (__m256d *)x;
 	__m256d *vy = (__m256d *)y;
-
+	
+	//足し算 
 	for(size_t i=0; i<end; ++i)
 		vz[i] = _mm256_add_pd(vx[i], vy[i]);
 }
 
-int main(void){
+int main(void)
+{
 	const size_t n = 1024;
 	double *x, *y, *z;
+	
+	//アラインメントは32=128bit
+	//double * nを32区切りで分割して_mm_mallocで確保
 
 	x = (double *)_mm_malloc(sizeof(double) * n, 32);
 	y = (double *)_mm_malloc(sizeof(double) * n, 32);
@@ -30,7 +39,9 @@ int main(void){
 	vec_add(n, z, x, y);
 
 	for(size_t i=0; i<n; ++i) printf("%g\n", z[i]);
+	
 
+	//_mm_mallocで確保したものは_mm_freeで開放
 	_mm_free(x);
 	_mm_free(y);
 	_mm_free(z);
